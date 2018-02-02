068d2119968c4646fa06662bb5449b42a7ddbda8
Daniel Muñoz Martín <27631697+danimm@users.noreply.github.com>
2018-02-02 08:57:04 +0100

    preparado para pasar la web a vue

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true, // Choose whether you can drag to open on touch screens,
-    }
-  );
+    });
 
   });
