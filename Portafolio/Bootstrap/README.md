2914bccc23f4e35aeff77695b45f7208900aeac4
Daniel Muñoz Martín <27631697+danimm@users.noreply.github.com>
2018-02-06 11:33:31 +0100

    web en vuejs

--- Portafolio/Bootstrap/README.md
+++
@@ -1,5 +0,0 @@
-# Welcome to my Personal Repository
-
-www.danimm.com    
-
-https://danimm.github.io/
--- Portafolio/Bootstrap/js/app.js
+++
@@ -1,6 +0,0 @@
-const $home = $('#iconMenu')
-const $navMenu = $('#navMenu')
-
-$home.click(function(ev){
-  $navMenu.addClass('icon-close')
-})
